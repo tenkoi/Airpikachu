42bcdc1114fc2e4f6ae7b3a2d48709b25cc08fae
MOHD FAIZ <tenkois@gmail.com>
2018-09-28 16:23:33 +0800

    gravatar not done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def avatar_url(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+		"http://www.gravatar.com/avatar/#{gravatar_id}.JPEG?d=identical&s=150"
+	end
+
+	def method_name
+		
+	end
 end
