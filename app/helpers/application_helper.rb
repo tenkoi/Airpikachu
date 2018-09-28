fbb10cc08d8611e513edaa150f82afa9a82b204a
MOHD FAIZ <tenkois@gmail.com>
2018-09-29 00:53:28 +0800

    gravatar done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
 
-	def avatar_url(user)
-		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-		"http://www.gravatar.com/avatar/#{gravatar_id}.JPEG?d=identical&s=150"
-	end
+	# def avatar_url(user)
+	# 	gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+	# 	"http://www.gravatar.com/avatar/#{gravatar_id}.JPEG?d=identical&s=150"
+	# end
 
-	def method_name
-		
+	
+	def avatar_url(user)
+	gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+	"http://gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=150 "
 	end
+ 
 end
