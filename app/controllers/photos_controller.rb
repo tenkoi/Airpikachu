abd4e286e0924af28ff1274de45ac2e6261bae2f
Faiz <tenkois@gmail.com>
2018-11-11 17:10:33 +0800

    need fix picture thumbnails

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,16 +2,16 @@ class PhotosController < ApplicationController
 
 
   def create
-    @create = Room.find(params[:room_id])
+    @room = Room.find(params[:room_id])
 
-    if params[:images]
-      params[:images].each do |img|
-        @room.photos.create(image: img)
-      end
+   if params[:images]
+     params[:images].each do |img|
+       @room.photos.create(image: img)
+     end
 
-      @photos = room.photos
-      redirect_back(fallback_location: request.referer, notice: "Saved...")
-      end
+     @photos = @room.photos
+     redirect_back(fallback_location: request.referer, notice: "Saved...")
+   end
 
 
 
